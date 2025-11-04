50572d912abf8c77598c577eca520012f62e1c88
ivandemido04 <ivandemidov04@yandex.ru>
2025-11-05 02:45:57 +0300

    fix pipeline settings

--- src/main/java/security/infseclab1/service/UserService.java
+++ src/main/java/security/infseclab1/service/UserService.java
@@ -13,9 +13,12 @@ import security.infseclab1.domain.dto.SetStatusRequest;
 import java.util.Objects;
 
 @Service
-@RequiredArgsConstructor
 public final class UserService {
     private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public User save(User user) {
         return userRepository.save(user);
